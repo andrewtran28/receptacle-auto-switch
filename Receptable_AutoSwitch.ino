b170b39807198c41b8ad0d57710e819754852f02
Andrew Tran <tran.andrew@outlook.com>
2025-09-08 18:04:24 -0400

    Feat: RELAYS stays on if SOURCE turns back on before RELAY_SHUTOFF_DELAY

--- Receptable_AutoSwitch.ino
+++ Receptable_AutoSwitch.ino
@@ -7,7 +7,7 @@
 
 #define CURRENT_THRESHOLD 3300    // The analog value above which the relay shall be triggered
 #define CURRENT_SAMPLE_PERIOD 500 // Time (ms) to sample the current reading
-#define RELAY_STARTUP_DELAY 1000  // Time (ms) to delay before activating relay after tool has started
+#define RELAY_STARTUP_DELAY 500   // Time (ms) to delay before activating relay after tool has started
 #define RELAY_SHUTOFF_DELAY 3000  // Time (ms) to delay shutting off the relay after the tool has been shutoff
 #define STARTUP_GRACE_PERIOD 1000 // Time (ms) to ignore current sensor (forces high threshold at boot)
 
@@ -15,6 +15,8 @@ int analogValue = 0;              // Stores ADC values read in from the current 
 unsigned long stopwatch = 0;      // Used to keep track of elapsed time
 boolean relayPinState = HIGH;     // Stores the current state of the relay pin
 unsigned long startupTime = 0;    // Stores the time the device was powered on
+unsigned long lastToolSeenOn = 0; // Tracks the last time tool current was above threshold
+bool toolIsOn = false;            // Tracks tool state
 
 void setup() {
   Serial.begin(9600);
@@ -41,16 +43,25 @@ void loop() {
     effectiveThreshold = 9999;   // Arbitrary high value to ensure relay does not trigger
   }
 
-  // If the max ADC value from the current sensor exceeds the effective threshold, activate the relay
-  if(analogValue > effectiveThreshold) {
-    delay(RELAY_STARTUP_DELAY);
-    relayPinState = RELAY_LOGIC;
+  // Detect tool state based on current sensor
+  if (analogValue > effectiveThreshold) {
+    toolIsOn = true;
+    lastToolSeenOn = millis();   // Update the last time tool was detected ON
+  } else {
+    toolIsOn = false;
   }
-  // Otherwise if the relay was just activated, wait for the predetermined shutoff delay and set the state to HIGH
-  else {
-    if(relayPinState == RELAY_LOGIC) {
-      delay(RELAY_SHUTOFF_DELAY);
-      relayPinState = !relayPinState;
+
+  // Relay control logic
+  if (toolIsOn) {
+    // If tool is on, make sure relay is activated after startup delay
+    if (relayPinState != RELAY_LOGIC) {
+      delay(RELAY_STARTUP_DELAY);  // Small blocking delay is ok for startup only
+      relayPinState = RELAY_LOGIC;
+    }
+  } else {
+    // Tool is OFF, but only shut off relay if the shutoff delay has fully elapsed
+    if (relayPinState == RELAY_LOGIC && millis() - lastToolSeenOn > RELAY_SHUTOFF_DELAY) {
+      relayPinState = !RELAY_LOGIC; // Turn relay OFF
     }
   }
 
